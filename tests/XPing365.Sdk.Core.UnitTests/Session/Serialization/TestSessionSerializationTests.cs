fd4183c90f7a2ec1efa5bdaf28f13437b5ab8969
Adrian <adydecki@gmail.com>
2024-03-09 22:02:01 +0100

    Add unit tests for serialization.

--- tests/XPing365.Sdk.Core.UnitTests/Session/Serialization/TestSessionSerializationTests.cs
+++ tests/XPing365.Sdk.Core.UnitTests/Session/Serialization/TestSessionSerializationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.Serialization;
+using System.Text;
 using System.Xml;
 using System.Xml.XPath;
 using XPing365.Sdk.Core.Common;
@@ -79,15 +80,23 @@ internal class TestSessionSerializationTests
         serializer.Serialize(session, stream, SerializationFormat.XML);
         stream.Position = 0;
 
-        using XmlReader xmlReader = XmlReader.Create(stream);
-        var document = new XPathDocument(xmlReader);
-        XPathNavigator navigator = document.CreateNavigator();
-        XPathNodeIterator nodes = navigator.Select(XPathExpression.Compile("//TestSession/Id"));
+        using var reader = new StreamReader(stream, Encoding.UTF8);
+
+        // Load the XML into an XmlDocument
+        var doc = new XmlDocument();
+        doc.Load(reader);
+        
+        // Create a NamespaceManager and add the namespace used in the XML
+        var namespaceManager = new XmlNamespaceManager(doc.NameTable);
+        namespaceManager.AddNamespace("ns", "http://schemas.datacontract.org/2004/07/XPing365.Sdk.Core.Session");
+
+        // Use the namespace prefix in the XPath expression to select the Id element
+        XmlNode? idNode = doc.SelectSingleNode("//ns:TestSession/Id", namespaceManager);
 
         // Assert
-        Assert.That(nodes.MoveNext(), Is.True);
-        Assert.That(nodes.Current, Is.Not.Null);
-        Assert.That(nodes.Current.Value, Is.EqualTo(session.Id.ToString()));
+        Assert.That(idNode, Is.Not.Null);
+        Assert.That(Guid.TryParse(idNode.InnerText, out _), Is.True);
+        Assert.That(Guid.Parse(idNode.InnerText), Is.EqualTo(session.Id));
     }
 
     [Test]
@@ -116,20 +125,29 @@ internal class TestSessionSerializationTests
         // Act
         serializer.Serialize(session, stream, SerializationFormat.XML);
         stream.Position = 0;
+        using var reader = new StreamReader(stream, Encoding.UTF8);
 
-        using XmlReader xmlReader = XmlReader.Create(stream);
-        var document = new XPathDocument(xmlReader);
-        XPathNavigator navigator = document.CreateNavigator();
-        XPathNodeIterator nodes = navigator.Select("/TestSession/Url");
+        // Load the XML into an XmlDocument
+        var doc = new XmlDocument();
+        doc.Load(reader);
+
+        // Create a NamespaceManager and add the namespace used in the XML
+        var namespaceManager = new XmlNamespaceManager(doc.NameTable);
+        namespaceManager.AddNamespace("ns", "http://schemas.datacontract.org/2004/07/XPing365.Sdk.Core.Session");
+
+        // Use the namespace prefix in the XPath expression to select the Url element
+        XmlNode? urlNode = doc.SelectSingleNode("//ns:TestSession/Url", namespaceManager);
 
         // Assert
-        Assert.That(nodes.MoveNext(), Is.True);
-        Assert.That(nodes.Current, Is.Not.Null);
-        Assert.That(nodes.Current.Value, Is.EqualTo(session.Url.AbsoluteUri));
+        Assert.That(urlNode, Is.Not.Null);
+        Assert.That(Uri.TryCreate(urlNode.InnerText, UriKind.Absolute, out _), Is.True);
+        Assert.That(new Uri(urlNode.InnerText), Is.EqualTo(session.Url));
     }
 
+    [TestCase(SerializationFormat.XML)]
+    [TestCase(SerializationFormat.Binary)]
     [Test]
-    public void TestSessionInstanceIsCorrectlyDeserializedFromXml()
+    public void TestSessionInstanceIsCorrectlyDeserialized(SerializationFormat format)
     {
         // Arrange
         TestSession session = new()
@@ -152,9 +170,9 @@ internal class TestSessionSerializationTests
         using var stream = new MemoryStream();
 
         // Act
-        serializer.Serialize(session, stream, SerializationFormat.XML);
+        serializer.Serialize(session, stream, format);
         stream.Position = 0;
-        TestSession? session1 = serializer.Deserialize(stream, SerializationFormat.XML);
+        TestSession? session1 = serializer.Deserialize(stream, format);
 
         // Assert
         Assert.That(session1, Is.Not.Null);
@@ -166,6 +184,33 @@ internal class TestSessionSerializationTests
         Assert.That(session1.Steps.First().PropertyBag, Is.Not.Null);
         Assert.That(session.Steps.First().StartDate, Is.EqualTo(session1.Steps.First().StartDate));
         Assert.That(session.Steps.First().Type, Is.EqualTo(session1.Steps.First().Type));
+    }
+
+    [TestCase(SerializationFormat.XML)]
+    [TestCase(SerializationFormat.Binary)]
+    [Test]
+    public void TestSessionIdIsCorrectlyDeserialized(SerializationFormat format)
+    {
+        // Arrange
+        TestSession session = new()
+        {
+            Url = new Uri("https://test"),
+            StartDate = DateTime.UtcNow,
+            State = TestSessionState.NotStarted,
+            Steps = []
+        };
+
+        var serializer = new TestSessionSerializer();
+        using var stream = new MemoryStream();
+
+        // Act
+        serializer.Serialize(session, stream, format);
+        stream.Position = 0;
+        TestSession? session1 = serializer.Deserialize(stream, format);
+
+        // Assert
+        Assert.That(session1, Is.Not.Null);
+        Assert.That(session.Id, Is.EqualTo(session1.Id));
     }
 
     [Test]
